378ce2a2024d117b3490ce14937a9ba9e931a403
santhosh1329 <sks141130@gmail.com>
2025-04-19 20:10:06 +0530

    modified workflow

--- main.tf
+++ main.tf
@@ -13,16 +13,16 @@ provider "aws" {
   region  = "ap-south-1"
 }
 
-resource "aws_key_pair" "sample_ec2_key" {
-  key_name   = "my-ec2-key"
-  public_key = file("~/.ssh/my-ec2-key.pub")  # adjust path as needed
-}
+#resource "aws_key_pair" "sample_ec2_key" {
+#  key_name   = "my-ec2-key"
+#  public_key = file("~/.ssh/my-ec2-key.pub")  # adjust path as needed
+#}
 
 resource "aws_instance" "thena_devops" {
   ami           = "ami-0f1dcc636b69a6438" 
   instance_type = "t2.micro"
   vpc_security_group_ids = ["sg-09f2ed42d97b5fa92"] 
-  key_name = aws_key_pair.sample_ec2_key.key_name
+  key_name = "my-ec2-key"
   associate_public_ip_address = true
 
   user_data = <<-EOF
